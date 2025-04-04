0a8c703e4756171ceda7ab30a59c1ea9fd90662c
berquerant <sin@sin-pro.local>
2025-04-04 23:40:03 +0900

    Version 0.2.0

--- tests/test_ai_roundtable.py
+++ tests/test_ai_roundtable.py
@@ -26,7 +26,7 @@ def test_e2e():
         [
             "pip",
             "install",
-            "dist/ai_roundtable-0.1.4.tar.gz",
+            "dist/ai_roundtable-0.2.0.tar.gz",
         ],
         pwd,
     )
